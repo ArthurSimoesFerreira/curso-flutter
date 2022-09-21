51f11fd8eaf9c30e013e6730a90c0c6ed7202c8f
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-09-21 13:57:05 -0300

    Passando uma Função do componente pai transaction_user.dart para o filho transaction_form.dart , e definindo que ela será chamada ao apertar o botão.
    Função para adicionar uma transação. E
    um setState para modificar o componente

--- expenses/lib/components/transaction_form.dart
+++ expenses/lib/components/transaction_form.dart
@@ -4,7 +4,9 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
-  TransactionForm({super.key});
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +33,9 @@ class TransactionForm extends StatelessWidget {
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0;
+                    onSubmit(title, value);
                   },
                   child: const Text(
                     "Nova Transação",
