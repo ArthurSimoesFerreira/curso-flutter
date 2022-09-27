196b6fee98e6beda7b632074f193843dc4299a6f
ArthurSimoesFerreira <asferreira1002@gmail.com>
2022-09-27 12:39:41 -0300

    Finalização do Chart Widget Método get _weekTotalValue para pegar a soma Total dos Valores Porcentagem = valor / _weekTotalValue Primeira implementação do Flexible e FlexBox

--- expenses/lib/components/chart_bar.dart
+++ expenses/lib/components/chart_bar.dart
@@ -16,15 +16,49 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("R\$${value.toStringAsFixed(2)}"),
+        FittedBox(
+          child: Text(
+            value.toStringAsFixed(2),
+            style: Theme.of(context).textTheme.titleSmall,
+          ),
+        ),
         const SizedBox(height: 5),
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            // Pintar o gráfico de baixo para cima
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  // Pôr a borda um pouco circular
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              // Pintar a barra
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
         const SizedBox(height: 5),
-        Text(label),
+        Text(
+          label,
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
       ],
     );
   }
